5ad7213496ca12d39b43e69ff90cc0317e9bd6fd
Nathan Persall <npersall@gmail.com>
2019-05-12 22:53:36 -0700

    Commit: Sun May 12 22:53:36 PDT 2019

--- bot.sh
+++ bot.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
-#
-# Deploy locally by adding the following line to your crontab:
-# 0 22 * * * source ~/Desktop/code/commit-bot/bot.sh
-#
-# Edit your crontab in vim w/ the simple command:
-# crontab -e
-#
-# Deploying just on your computer is better than a server if you want
-# your commits to more realistically mirror your computer usage.
-#
-# ...c'mon, nobody commits EVERY day ;)
-#
 info="Commit: $(date)"
 
 cd ~/Desktop/code/commit-bot/
-echo "$info" >> output.txt
+echo "$info" >> commit.txt
 echo "$info"
 echo
 
